088bd6268a3a52630a0ac8b14cbb92079d57a531
Jeff Keopp <keopp@cray.com>
2018-09-18 12:56:07 -0500

    UAN-238: Change uas-mgr to use k8s service user
    Removed kube-cfg secret as it is no longer used.

--- swagger_server/uas_lib/uan_mgr.py
+++ swagger_server/uas_lib/uan_mgr.py
@@ -130,19 +130,13 @@ class UanManager(object):
                      name='UAN_PUBKEY',
                      value=usersshpubkey.read().decode())],
             ports=[client.V1ContainerPort(container_port=30123)],
-            volume_mounts = [client.V1VolumeMount(name='kube-cfg',
-                                                  mount_path='/etc/kube',
-                                                  read_only=True),
-                             client.V1VolumeMount(name='scratch',
+            volume_mounts = [client.V1VolumeMount(name='scratch',
                                                   mount_path='/scratch')])
         # Create a volumes template
-        volumes = [client.V1Volume(name='kube-cfg',
-                                   secret=client.V1SecretVolumeSource(
-                                       secret_name='kube-cfg')),
-                   client.V1Volume(name='scratch',
+        volumes = [client.V1Volume(name='scratch',
                                    host_path=client.V1HostPathVolumeSource(
-                                       path='/scratch',
-                                       type='DirectoryOrCreate'))]
+                                   path='/scratch',
+                                   type='DirectoryOrCreate'))]
         # Create and configure a spec section
         template = client.V1PodTemplateSpec(
             metadata=client.V1ObjectMeta(labels={"app": deployment_name}),
