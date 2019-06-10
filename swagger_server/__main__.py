710655ccaffc30e9595a1f5abe27684564de79e8
Alan Mutschelknaus <alanm@cray.com>
2019-06-10 14:02:09 -0500

    CASMUSER-1207: Change port to 8088 and fix ansible
    Testing on slice showed that 8080 conflicted with another service
    when cray_uas_mgr ran behind istio. Change ports to 8088.
    
    Also fix a renamed file in the localize play.

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,7 +9,7 @@ def main():
     app = connexion.App(__name__)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Cray User Access Service'}, base_path='/v1')
-    app.run(port=8080)
+    app.run(port=8088)
 
 
 if __name__ == '__main__':
