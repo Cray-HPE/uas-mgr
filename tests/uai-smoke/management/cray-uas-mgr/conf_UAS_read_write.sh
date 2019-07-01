e6d94924522c75659d42260de7c1c4e1960e681a
Tom Choi <tchoi@cray.com>
2019-07-01 10:51:27 -0500

    Move one of UAI tests, conf_UAS_read_write.sh, from Health Check to CT

---
+++ tests/uai-smoke/management/cray-uas-mgr/conf_UAS_read_write.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Copyright 2018, 2019 Cray Inc. All Rights Reserved.
+#
+###############################################################
+#
+#     OS Testing - Cray Inc.
+#
+#     TEST IDENTIFIER   : conf_UAS_read_write
+#
+#     TEST TITLE        : Simple Read Write Test for Confidence Suite
+#
+#     DESIGN DESCRIPTION
+#       These test cases are executed from the UAS.  The tests will
+#       verify we can R/W files
+#
+###############################################################
+
+set -o errexit
+set -o xtrace
+
+: "${TMPDIR:=/tmp}"
+
+hostname=$(hostname)
+line="Confidence Test Suite test file on $hostname"
+echo ${line} > $TMPDIR/confidencetest$$.txt
+
+# put contents of confidencetest.txt into variable fileContents
+fileContents=$(cat $TMPDIR/confidencetest$$.txt)
+
+if [[ $line == $fileContents ]] ; then
+    echo "PASS  The contents of $TMPDIR/confidencetest$$.txt are as expected."
+else
+    echo "FAIL  The contents of $TMPDIR/confidencetest$$.txt are not as expected. fileContents=$fileContents."
+fi
+
+rm $TMPDIR/confidencetest$$.txt
