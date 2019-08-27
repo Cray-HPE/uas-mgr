26de0bc86d7fbb24881667f3e3c4ee36c06f547d
Jeff Keopp <keopp@cray.com>
2019-08-27 15:42:00 -0500

    CASMUSER-1400: Initial work to move to LoadBalancer with BGP

--- swagger_server/uas_lib/uai_mgr.py
+++ swagger_server/uas_lib/uai_mgr.py
@@ -346,12 +346,14 @@ class UaiManager(object):
                                                 e.reason))
             return uai
 
-        uai.uai_ip = self.uas_cfg.get_external_ip()
-        for srv_port in srv_resp.spec.ports:
-            if srv_port.port in self.uas_cfg.get_valid_optional_ports():
-                uai.uai_portmap[srv_port.port] = srv_port.node_port
-            else:
-                uai.uai_port = srv_port.node_port
+        if srv_resp:
+            # uai.uai_ip = srv_resp.spec.load_balancer_ip
+            uai.uai_ip = self.uas_cfg.get_external_ip()
+            for srv_port in srv_resp.spec.ports:
+                if srv_port.port in self.uas_cfg.get_valid_optional_ports():
+                    uai.uai_portmap[srv_port.port] = srv_port.target_port
+                else:
+                    uai.uai_port = srv_port.target_port
         uai.uai_connect_string = self.gen_connection_string(uai)
         return uai
 
