0277208dfc5eccd45a78fd485e6f2af182360b09
Tom Choi <tchoi@cray.com>
2019-10-08 15:17:09 -0500

    CASMUSER-2345: Test uas-mgr version

--- tests/sms-smoke/user/cray-uas-mgr/uas-smoke.sh
+++ tests/sms-smoke/user/cray-uas-mgr/uas-smoke.sh
@@ -10,9 +10,19 @@ set -e
 # check_pod_status is defined at /opt/cray/tests/sms-resources/bin/
 check_pod_status cray-uas-mgr
 if [[ $? == 0 ]]; then
-    echo "PASS: Verify that cray-uas-mgr pod exists on a system"
+    echo "SUCCESS: Verify that cray-uas-mgr pod exists on a system"
 else
     echo "FAIL: cray-uas-mgr pod doesn't exist on a system."
+    exit 1
+fi
+
+# Test uas-mgr version
+OUT_UAS_MGR_VER=$(rpm -q cray-uas-mgr-crayctldeploy)
+if [[ $? == 0 ]]; then
+    echo "SUCCESS: Get uas-mgr version, $OUT_UAS_MGR_VER"
+else
+    echo "FAIL: Cannot get uas-mgr version"
+    echo "$OUT_UAS_MGR_VER"
     exit 1
 fi
 
