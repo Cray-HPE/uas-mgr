9cd30113a4292415ffa5ca31fd6406542c6ed629
Alan Mutschelknaus <alanm@cray.com>
2020-04-09 08:46:23 -0500

    CASMUAS-82: Update build trigger to use 1.3 CME
    In order to use the correct version of CME rpm macros update to
    use SLES_15_SP1 in the build triggers.

--- uasBuildPrep.sh
+++ uasBuildPrep.sh
@@ -6,5 +6,5 @@
 
 # Add repo for build macros
 zypper --non-interactive addrepo --no-gpgcheck \
-    http://car.dev.cray.com/artifactory/shasta-premium/SHASTA-OS/sle15_ncn/x86_64/dev/master/ \
+    http://car.dev.cray.com/artifactory/shasta-premium/SHASTA-OS/sle15_sp1_ncn/x86_64/dev/master/ \
     shasta-os-build-resource
