c5af851301369bb9e2d36189988f3a7d9f8f98d9
Eric Lund <erl@cray.com>
2020-06-03 09:49:31 -0500

    CASMUAS-7 removed unnecessary content in uas_data_model/__init__.py

--- swagger_server/uas_data_model/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Data Models for UAS Config
-
-Copyright 2020, Cray Inc. All rights reserved.
-
-"""
-#from swagger_server.uas_data_model.uai_image import UAIImage
-#from swagger_server.uas_data_model.uai_volume import UAIVolume
