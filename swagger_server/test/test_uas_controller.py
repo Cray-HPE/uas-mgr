0f3e8645f4aa1aeea4bbba4038d065e9cb885b03
Eric Lund <erl@cray.com>
2020-10-26 15:25:28 -0500

    CASMUAS-182 Added unit tests for classes

--- swagger_server/test/test_uas_controller.py
+++ swagger_server/test/test_uas_controller.py
@@ -131,12 +131,13 @@ class TestUasController(unittest.TestCase):
             vols = uas_ctl.get_uas_volumes_admin()
         self.assertIsInstance(vols, list)
 
-    def __create_test_volume(self):
+    def __create_test_volume(self, volume_name=None):
         """Create a test volume through the API and make sure that works,
         return the volume ID so that it can be used for subsequent
         activities.
 
         """
+        volume_name = "my-volume" if volume_name is None else volume_name
         vol_desc = io.BytesIO()
         vol_desc.write(
             bytes(
@@ -152,7 +153,7 @@ class TestUasController(unittest.TestCase):
         )
         with app.test_request_context('/'):
             resp = uas_ctl.create_uas_volume_admin(
-                volumename="my-volume",
+                volumename=volume_name,
                 mount_path="/var/mnt",
                 volume_description=vol_desc
             )
@@ -362,23 +363,178 @@ class TestUasController(unittest.TestCase):
             with self.assertRaises(werkzeug.exceptions.NotFound):
                 _ = uas_ctl.delete_uas_resource_admin(resource_id=str(uuid4()))
 
+    def __create_test_uai_class(self):
+        """Create a test class through the API that is set up for
+        creating UAIs and make sure that works, return the class ID
+        so that it can be used for subsequent activities.
+
+        """
+        comment = "Test UAI Class"
+        default = True
+        public_ssh = False
+        image_id = self.__create_test_image("Test image for test class")
+        priority_class_name = "test-priority-class"
+        namespace = "test-namespace"
+        uai_creation_class = None
+        resource_id = self.__create_test_resource()
+        volume_list = [self.__create_test_volume("my-uai-volume")]
+        with app.test_request_context('/'):
+            resp = uas_ctl.create_uas_class_admin(
+                comment=comment,
+                default=default,
+                public_ssh=public_ssh,
+                image_id=image_id,
+                priority_class_name=priority_class_name,
+                namespace=namespace,
+                uai_creation_class=uai_creation_class,
+                resource_id=resource_id,
+                volume_list=volume_list
+            )
+        self.assertIsInstance(resp, dict)
+        self.assertIn('class_id', resp)
+        return resp['class_id']
+
+    def __create_test_broker_class(self):
+        """Create a test class through the API that is set up for
+        creating Brokers and make sure that works, return the class ID
+        so that it can be used for subsequent activities.
+
+        """
+        comment = "Test Broker Class"
+        default = False
+        public_ssh = True
+        image_id = self.__create_test_image("Test image for broker class")
+        priority_class_name = "test-broker-priority-class"
+        namespace = "test-broker-namespace"
+        uai_creation_class = self.__create_test_uai_class()
+        resource_id = self.__create_test_resource()
+        volume_list = [self.__create_test_volume("my-broker-volume")]
+        with app.test_request_context('/'):
+            resp = uas_ctl.create_uas_class_admin(
+                comment=comment,
+                default=default,
+                public_ssh=public_ssh,
+                image_id=image_id,
+                priority_class_name=priority_class_name,
+                namespace=namespace,
+                uai_creation_class=uai_creation_class,
+                resource_id=resource_id,
+                volume_list=volume_list
+            )
+        self.assertIsInstance(resp, dict)
+        self.assertIn('class_id', resp)
+        return resp['class_id']
+
+    def __delete_test_class(self, class_id):
+        """Delete a class by its class_id and verify the result.
+
+        """
+        with app.test_request_context('/'):
+            resp = uas_ctl.get_uas_class_admin(class_id=class_id)
+            self.assertIn('class_id', resp)
+            self.assertEqual(class_id, resp['class_id'])
+            uai_creation_class = resp.get('uai_creation_class', None)
+            resource_id = resp.get('resource_id', None)
+            volume_list = resp.get('volume_list', [])
+            image_id = resp.get("image_id", None)
+            if image_id:
+                self.__delete_test_image(image_id)
+            if resource_id:
+                self.__delete_test_resource(resource_id)
+            if uai_creation_class:
+                self.__delete_test_class(uai_creation_class)
+            for volume_id in volume_list:
+                self.__delete_test_volume(volume_id)
+            resp = uas_ctl.delete_uas_class_admin(class_id=class_id)
+        self.assertIn('class_id', resp)
+        self.assertEqual(class_id, resp['class_id'])
+
+    # pylint: disable=missing-docstring
+    def test_get_uas_class_admin(self):
+        with app.test_request_context('/'):
+            resp = uas_ctl.get_uas_class_admin(class_id=None)
+            self.assertEqual(resp, "Must provide class_id (UUID) to get.")
+            with self.assertRaises(werkzeug.exceptions.NotFound):
+                _ = uas_ctl.get_uas_class_admin(class_id=str(uuid4()))
+
+    # pylint: disable=missing-docstring
+    def test_update_uas_class_admin(self):
+        with app.test_request_context('/'):
+            resp = uas_ctl.update_uas_class_admin(class_id=None)
+            self.assertEqual(resp, "Must provide class_id of the UAI Class to update.")
+            with self.assertRaises(werkzeug.exceptions.NotFound):
+                _ = uas_ctl.update_uas_class_admin(class_id=str(uuid4()))
+            class_id = self.__create_test_broker_class()
+            comment = "Test Broker Class -- edited comment"
+            default = False
+            public_ssh = True
+            priority_class_name = "test-broker-priority-other-class"
+            namespace = "test-broker--other-namespace"
+            resp = uas_ctl.update_uas_class_admin(
+                class_id=class_id,
+                comment=comment,
+                default=default,
+                public_ssh=public_ssh,
+                priority_class_name=priority_class_name,
+                namespace=namespace
+            )
+            self.assertIsInstance(resp, dict)
+            self.assertIn('class_id', resp)
+            self.assertEqual(class_id, resp['class_id'])
+            self.assertIn('comment', resp)
+            self.assertEqual(comment, resp['comment'])
+            self.assertIn('default', resp)
+            self.assertEqual(default, resp['default'])
+            self.assertIn('public_ssh', resp)
+            self.assertEqual(public_ssh, resp['public_ssh'])
+            self.assertIn('priority_class_name', resp)
+            self.assertEqual(
+                priority_class_name,
+                resp['priority_class_name']
+            )
+            self.assertIn('namespace', resp)
+            self.assertEqual(namespace, resp['namespace'])
+            self.__delete_test_class(class_id)
+
+    # pylint: disable=missing-docstring
+    def test_delete_uas_class_admin(self):
+        with app.test_request_context('/'):
+            resp = uas_ctl.delete_uas_class_admin(class_id=None)
+            self.assertEqual(resp, "Must provide class_id of UAI Class to delete.")
+            resp = uas_ctl.delete_uas_class_admin(class_id="")
+            self.assertEqual(resp, "Must provide class_id of UAI Class to delete.")
+            with self.assertRaises(werkzeug.exceptions.NotFound):
+                _ = uas_ctl.delete_uas_class_admin(class_id=str(uuid4()))
+
     # pylint: disable=missing-docstring
     def test_delete_local_config_admin(self):
         with app.test_request_context('/'):
             # Make sure there is something in the local configuration
-            resource_id = self.__create_test_resource()
-            resp = uas_ctl.get_uas_resource_admin(resource_id=resource_id)
-            self.assertIn('resource_id', resp)
-            self.assertEqual(resource_id, resp['resource_id'])
+            volume_id = self.__create_test_volume()
+            resp = uas_ctl.get_uas_volume_admin(volume_id=volume_id)
+            self.assertIn('volume_id', resp)
+            self.assertEqual(volume_id, resp['volume_id'])
             image_id = self.__create_test_image("locally_configured_image")
             resp = uas_ctl.get_uas_image_admin(image_id=image_id)
             self.assertIn('image_id', resp)
             self.assertEqual(image_id, resp['image_id'])
+            resource_id = self.__create_test_resource()
+            resp = uas_ctl.get_uas_resource_admin(resource_id=resource_id)
+            self.assertIn('resource_id', resp)
+            self.assertEqual(resource_id, resp['resource_id'])
+            class_id = self.__create_test_uai_class()
+            resp = uas_ctl.get_uas_class_admin(class_id=class_id)
+            self.assertIn('class_id', resp)
+            self.assertEqual(class_id, resp['class_id'])
             resp = uas_ctl.delete_local_config_admin()
+            with self.assertRaises(werkzeug.exceptions.NotFound):
+                _ = uas_ctl.get_uas_volume_admin(volume_id=volume_id)
+            with self.assertRaises(werkzeug.exceptions.NotFound):
+                _ = uas_ctl.get_uas_image_admin(image_id=image_id)
             with self.assertRaises(werkzeug.exceptions.NotFound):
                 _ = uas_ctl.get_uas_resource_admin(resource_id=resource_id)
             with self.assertRaises(werkzeug.exceptions.NotFound):
-                _ = uas_ctl.get_uas_image_admin(image_id=image_id)
+                _ = uas_ctl.get_uas_class_admin(class_id=class_id)
 
 
 if __name__ == '__main__':
