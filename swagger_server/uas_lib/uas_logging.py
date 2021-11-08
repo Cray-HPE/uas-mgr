d39add39695a904fb04f7cd05b021c53eb09c897
Eric Lund <eric.lund@hpe.com>
2021-11-08 11:16:22 -0600

    CASMUSER-2810 allow changing log levels from environment

--- swagger_server/uas_lib/uas_logging.py
+++ swagger_server/uas_lib/uas_logging.py
@@ -22,20 +22,51 @@
 """ Logging for UAS operations.  Import `logger` from here to get logging in your code.
 
 """
+import os
 import sys
 import logging
 
+# Learn any environment setting that might be presented for logging level.  Default to
+# 'info' and use 'info' if a bad setting is requested.  If a bad level is requested,
+# capture the level and log it once we have logging set up.
+__LEVEL_MAP = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+}
+#pylint: disable=invalid-name
+env_level = os.environ.get("UAS_LOGGING_LEVEL", 'info')
+#pylint: disable=invalid-name
+bad_env_level = None if env_level in __LEVEL_MAP else env_level
+#pylint: disable=invalid-name
+env_level = env_level if not bad_env_level else 'info'
+
+# pylint: disable=invalid-name
 logger = logging.getLogger('uas_mgr')
-logger.setLevel(logging.INFO)
+logger.setLevel(__LEVEL_MAP[env_level])
 # pylint: disable=invalid-name
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
+handler.setLevel(__LEVEL_MAP[env_level])
 # pylint: disable=invalid-name
 formatter = logging.Formatter(
     "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+# Determine and log the setting of the log level so that developers can
+# see if their attempts to change the log level worked and so that the
+# expected level of logging will be known.
+__BAD_LEVEL_LOG = (
+    "UAS_LOGGING_LEVEL set to invalid log level '%s' ignoring that value "
+    "and using '%s'" % (bad_env_level, env_level)
+)
+__GOOD_LEVEL_LOG = "setting logging level to '%s'" % env_level
+__LOG_LEVEL_LOG = (
+    __GOOD_LEVEL_LOG if bad_env_level is None else __BAD_LEVEL_LOG
+)
+logger.info(__LOG_LEVEL_LOG)
 
 def set_log_level(level):
     """ Set the logging level to a new value.
